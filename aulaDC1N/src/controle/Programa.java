3aa4aaba957ea0fa7d7ab716c3916e52a8c78f2a
AlexandreLouzada <professorlouzada@gmail.com>
2021-08-12 22:16:34 -0300

    Create Programa.java

---
+++ aulaDC1N/src/controle/Programa.java
@@ -0,0 +1,28 @@
+package controle;
+import java.util.Scanner;
+import dominio.Curso;
+import dominio.Professor;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		String matricula, cpf, nome;
+		System.out.println("CADASTRO DE CURSO");
+		System.out.print("Digite o código: ");
+		int codigo = leia.nextInt();
+		System.out.print("Digite a descrição: ");
+		String descricao = leia.next();
+		Curso curso = new Curso(codigo, descricao);
+		System.out.println("CADASTRO DE PROFESSORES");
+		for (int i = 0; i <= 2; i++) {
+			System.out.print("Digite a matricula: ");
+			matricula = leia.next();
+			System.out.print("Digite o CPF: ");
+			cpf = leia.next();
+			System.out.print("Digite o nome: ");
+			nome = leia.next();
+			Professor prof = new Professor(matricula, cpf, nome);
+			curso.addProfessor(prof);
+		}
+	}
+}
