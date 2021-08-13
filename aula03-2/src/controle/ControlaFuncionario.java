e2385a3b4e92475a5aab53db9280af977bb714a4
AlexandreLouzada <professorlouzada@gmail.com>
2021-08-12 22:15:30 -0300

    Create ControlaFuncionario.java

---
+++ aula03-2/src/controle/ControlaFuncionario.java
@@ -0,0 +1,21 @@
+package controle;
+import java.util.Scanner;
+import dominio.Funcionario;
+
+public class ControlaFuncionario {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.print("Digite o nome do funcionário: ");
+		String nome = leia.nextLine();
+		System.out.print("Digite o salário base: R$ ");
+		float salarioBase = leia.nextFloat();
+
+		Funcionario funcionario = new Funcionario(nome,salarioBase);
+        System.out.println("Desconto - INSS: R$ " + funcionario.descontoInss());
+		System.out.println("Gratificação   : R$ " + funcionario.gratificacao());
+		System.out.println("Salário líquido: R$ " + funcionario.salarioFinal());
+	}
+
+}
