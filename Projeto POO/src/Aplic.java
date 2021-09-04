bb9481b0c9b11abda224dce589577cf0934f37ce
AlexandreLouzada <professorlouzada@gmail.com>
2021-09-04 10:35:48 -0300

    Atualização Projeto JavaQuest
    Ajustando alguns exemplos

--- Projeto POO/src/Aplic.java
+++ Projeto POO/src/Aplic.java
@@ -1,4 +1,4 @@
-public class App {
+public class Aplic {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
--- testando/src/App.java
+++ testando/src/App.java
@@ -1,15 +1,7 @@
 public class App {
     public static void main(String[] args) {
-        int[] array = { 10, 20, 30 };
-        int b = 0;
-        try {
-            System.out.println("1");
-            int c = (array[3] / b);
-            System.out.println("2");
-        } catch (ArithmeticException ex) {
-            System.out.println("Exceção Aritmética");
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Índice de matriz fora dos limites");
+        int[] arr = new int[2];
+        for(int i=0; i < arr.length; i++){
+           System.out.print("hai ");
         }
-    }
-}
+ }
