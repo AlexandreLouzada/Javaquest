37cd38174af6ca416b42b7dace514733225fb01b
AlexandreLouzada <professorlouzada@gmail.com>
2021-09-04 10:44:19 -0300

    Atualização do Projeto JavaQuest
    Ajustando os exemplos

--- Projeto POO/src/Aplic.java
+++
@@ -1,5 +0,0 @@
-public class Aplic {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- testando/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        int[] arr = new int[2];
-        for(int i=0; i < arr.length; i++){
-           System.out.print("hai ");
-        }
- }
