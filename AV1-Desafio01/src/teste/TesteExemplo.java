cf5aba03ef560f4fcc28544f8b7af53a3f2bfae6
AlexandreLouzada <professorlouzada@gmail.com>
2022-03-16 19:49:33 -0300

    Ajustes nos projetos
    Ajustes nos projetos

---
+++ AV1-Desafio01/src/teste/TesteExemplo.java
@@ -0,0 +1,11 @@
+package teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteExemplo {
+	@Test
+	public void testeConfiguracao(){
+		Assert.assertEquals(2, (1+1), 0);
+	}
+}
